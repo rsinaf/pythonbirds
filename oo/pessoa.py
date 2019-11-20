8e29ea618b28155cdf910915dc953ff3a6f7c260
rsinaf <rsinaf@gmail.com>
2019-11-20 12:57:12 -0500

    added instance attributes nome and idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Hello'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
+    print(p.idade)
+
